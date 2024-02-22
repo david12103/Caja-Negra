7ef421e4412e7f5a09e3c8624b4fc06abc3c2b52
david12103 <davidps4gm@gmail.com>
2024-02-22 10:14:06 +0100

    Create TestPiedraPapelTijeras.java

---
+++ src/piedarapapeltiejeras08/TestPiedraPapelTijeras.java
@@ -0,0 +1,19 @@
+package piedarapapeltiejeras08;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TestPiedraPapelTijeras {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
